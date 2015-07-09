cc552e9c982439529eae283395417f2512b4cea1
RARomano <rodrigo@rodrigoromano.net>
2015-07-09 15:40:40 -0300

    First Commit

---
+++ WebCast.TimerJobSPOnline/WebCast.TimerJobSPOnline/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebCast.TimerJobSPOnline.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
---
+++ WebCast.TimerJobSPOnline/WebCast.TimerJobSPOnline/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebCast.TimerJobSPOnline.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
